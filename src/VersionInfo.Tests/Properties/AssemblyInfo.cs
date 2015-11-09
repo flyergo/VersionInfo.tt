179b1830ed6c54adb9654803e30827bc01b702c2
Flyergo <flyergo-one@web.de>
2015-11-09 20:32:17 +0100

    Added VersionInfo.tt and testing enviroment

---
+++ src/VersionInfo.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("VersionInfo.Tests")]
+[assembly: AssemblyProduct("VersionInfo.Tests")]
+
---
+++ src/VersionInfo.Tests/VersionInfoTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace VersionInfo.Tests
+{
+    [TestClass]
+    public class VersionInfoTests
+    {
+        [TestMethod]
+        public void GetMajorVersion()
+        {
+            var assemblyName = Assembly.GetExecutingAssembly().GetName();
+
+            Assert.IsTrue(assemblyName.Version.Major >= 0);
+        }
+
+        [TestMethod]
+        public void GetMinorVersion()
+        {
+            var assemblyName = Assembly.GetExecutingAssembly().GetName();
+
+            Assert.IsTrue(assemblyName.Version.Minor >= 0);
+        }
+
+        [TestMethod]
+        public void GetPatchVersion()
+        {
+            var assemblyName = Assembly.GetExecutingAssembly().GetName();
+
+            Assert.IsTrue(assemblyName.Version.Revision >= 0);
+        }
+
+        [TestMethod]
+        public void GetBuildVersion()
+        {
+            var assemblyName = Assembly.GetExecutingAssembly().GetName();
+
+            Assert.IsTrue(assemblyName.Version.Build >= 0);
+        }
+
+        [TestMethod]
+        public void GetVersion()
+        {
+            var assemblyName = Assembly.GetExecutingAssembly().GetName();
+
+            Assert.IsNotNull(assemblyName.Version);
+        }
+
+        [TestMethod]
+        public void GetFileVersion()
+        {
+            var fileVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+
+            Assert.IsNotNull(fileVersion.FileVersion);
+        }
+
+        [TestMethod]
+        public void GetProductVersion()
+        {
+            var fileVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+
+            Assert.IsNotNull(fileVersion.ProductVersion);
+        }
+    }
+}
